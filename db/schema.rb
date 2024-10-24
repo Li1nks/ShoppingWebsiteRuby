d25d67798c2b87c8892e2050737b16cfa624686d
Li1nks <michaelnathanielwatkins@gmail.com>
2024-10-24 13:59:04 -0400

    Added tiles to shopping home page to showcase random food items that someone can buy, background image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_24_030253) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_24_143013) do
   create_table "products", force: :cascade do |t|
     t.string "product_name"
     t.string "product_id"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_24_030253) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.string "image_url"
   end
 
   create_table "users", force: :cascade do |t|
